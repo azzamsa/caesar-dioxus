68573fa1cbd93b319a5a2f56a55844ecf91bece6
azzamsa <me@azzamsa.com>
2022-07-06 07:52:25 +0700

    fix: align heart icon

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -6,12 +6,13 @@ pub fn footer(cx: Scope) -> Element {
     cx.render(rsx!(
         section { class: "max-w-lg mx-auto flex justify-center text-white font-medium",
               a { class: "duration-500 transform hover:-translate-y-1 hover:scale-125 hover:underline",
-                  href: "#",
+                  href: "https://azzamsa.com/support/",
                   "Support Me"
-              }
-                  i { class: "inline-block mx-1 align-middle w-4 h-4",
+
+                  i { class: "inline-block mx-1 w-4 h-4",
                       icons::heart {}
                   }
+              }
               span { class: "mx-3",
                      "\u{2022}",
               }
