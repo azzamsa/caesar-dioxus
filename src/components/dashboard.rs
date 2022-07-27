2ccfb61d36e4972c3bd055970f168920d4b629d9
azzamsa <me@azzamsa.com>
2022-07-27 12:06:46 +0700

    refactor: simplify the state logic

--- src/components/dashboard.rs
+++ src/components/dashboard.rs
@@ -1,102 +1,74 @@
-use dioxus::prelude::*;
+use dioxus::{events::*, prelude::*};
 use nrot::{rot, rot_letter, Mode};
 
-#[derive(Clone, PartialEq)]
-pub struct SecretState {
-    input: String,
-    output: String,
+fn encrypt(plain: String) -> String {
+    let rotation = 13;
+    let plain_length = plain.len();
+    let plain_bytes = plain.as_bytes();
+
+    if plain_length == 1 {
+        let byte_result = rot_letter(Mode::Encrypt, plain_bytes[0], rotation);
+        format!("{}", String::from_utf8_lossy(&[byte_result]))
+    } else {
+        let bytes_result = rot(Mode::Encrypt, plain_bytes, rotation);
+        format!("{}", String::from_utf8_lossy(&bytes_result))
+    }
 }
 
-impl SecretState {
-    fn new() -> Self {
-        Self {
-            input: "".to_string(),
-            output: "".to_string(),
-        }
-    }
-    fn encrypt(&mut self, input: String) {
-        let rotation = 13;
-        let input_length = input.len();
-        let input_bytes = input.as_bytes();
-        self.input = input.clone();
+fn decrypt(secret: String) -> String {
+    let rotation = 13;
+    let secret_length = secret.len();
+    let secret_bytes = secret.as_bytes();
 
-        self.output = if input_length == 1 {
-            let byte_result = rot_letter(Mode::Encrypt, input_bytes[0], rotation);
-            format!("{}", String::from_utf8_lossy(&[byte_result]))
-        } else {
-            let bytes_result = rot(Mode::Encrypt, input_bytes, rotation);
-            format!("{}", String::from_utf8_lossy(&bytes_result))
-        };
-    }
-    fn decrypt(&mut self, output: String) {
-        let rotation = 13;
-        let output_length = output.len();
-        let output_bytes = output.as_bytes();
-        self.output = output.clone();
-
-        self.input = if output_length == 1 {
-            let byte_result = rot_letter(Mode::Decrypt, output_bytes[0], rotation);
-            format!("{}", String::from_utf8_lossy(&[byte_result]))
-        } else {
-            let bytes_result = rot(Mode::Decrypt, output_bytes, rotation);
-            format!("{}", String::from_utf8_lossy(&bytes_result))
-        };
+    if secret_length == 1 {
+        let byte_result = rot_letter(Mode::Decrypt, secret_bytes[0], rotation);
+        format!("{}", String::from_utf8_lossy(&[byte_result]))
+    } else {
+        let bytes_result = rot(Mode::Decrypt, secret_bytes, rotation);
+        format!("{}", String::from_utf8_lossy(&bytes_result))
     }
 }
 
 pub fn dashboard(cx: Scope) -> Element {
-    let secret = use_state(&cx, SecretState::new);
+    let plain = use_state(&cx, || "".to_string());
+    let secret = use_state(&cx, || "".to_string());
+
+    let on_input_plain = move |e: FormEvent| {
+        plain.set(e.value.clone());
+        let secret_str = encrypt(e.value.clone());
+        secret.set(secret_str);
+    };
+    let on_input_secret = move |e: FormEvent| {
+        secret.set(e.value.clone());
+        let plain_str = decrypt(e.value.clone());
+        plain.set(plain_str);
+    };
 
     cx.render(rsx!(
         section { class: "flex flex-col mt-10 ",
-                  input_textarea(secret: secret.clone()),
+                  div { class: "mb-6 pt-3 rounded bg-gray-200",
+                        label { class: "input-label",
+                                "Plain",
+                        },
+                        // plain textarea
+                        textarea { class: "input",
+                                   placeholder: "me@casar.tld",
+                                   value: "{plain}",
+                                   oninput: on_input_plain
+                        }
+                  }
                   div { class: "flex justify-center" }
-                  output_textarea(secret: secret.clone()),
-            }
-    ))
-}
-
-#[derive(PartialEq, Props)]
-pub struct SecretProps {
-    secret: UseState<SecretState>,
-}
-
-pub fn input_textarea(cx: Scope<SecretProps>) -> Element {
-    let secret = &cx.props.secret;
-
-    cx.render(rsx!(
-        div { class: "mb-6 pt-3 rounded bg-gray-200",
-              id: "input",
-              label { class: "input-label",
-                      "Secret",
-              },
-              textarea { class: "input",
-                         placeholder: "me@casar.tld",
-                         value: "{secret.input}",
-                         oninput: move |e| {
-                             secret.make_mut().encrypt(e.value.clone());
-                         }
-              }
-            }
-    ))
-}
-
-pub fn output_textarea(cx: Scope<SecretProps>) -> Element {
-    let secret = &cx.props.secret;
-
-    cx.render(rsx!(
-        div { class: "mb-6 pt-3 rounded bg-gray-200",
-              label { class: "input-label",
-                      "Encrypted",
-              },
-              textarea { class: "input",
-                         placeholder: "zr@pnfne.gyq",
-                         value: "{secret.output}",
-                         oninput: move |e| {
-                             secret.make_mut().decrypt(e.value.clone());
-                         }
-
-              }
-            }
+                  div { class: "mb-6 pt-3 rounded bg-gray-200",
+                        label { class: "input-label",
+                                "Plain",
+                        },
+                        // secret textarea
+                        textarea { class: "input",
+                                   placeholder: "me@casar.tld",
+                                   value: "{secret}",
+                                   oninput: on_input_secret
+                        }
+                  }
+        }
     ))
 }
